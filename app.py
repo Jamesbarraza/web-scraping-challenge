7e953155a6190e27753a48a24ff13ec8a8f2b53e
Jamesbarraza <59000118+Jamesbarraza@users.noreply.github.com>
2020-04-27 21:37:40 -0500

    final commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+# Scrape Route to Import `scrape_mars.py` Script & Call `scrape` Function
+@app.route("/scrape")
+def scrapper():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    #return "Scraping Successful"
+    return redirect("/")
+
+# Define Main Behavior
+if __name__ == "__main__":
+    app.run(debug=True)
